0ec63652290a80994175008d26dfef39f51f5caf
Gary Uppiah <Gary Uppiah>
2016-10-17 14:14:50 -0400

    sakila

--- 3.DML/Lubumbashi-Gary.sql
+++ 3.DML/Lubumbashi-Gary.sql
@@ -3,7 +3,7 @@
 
 ```
 select customer.last_name as Nom, sum(payment.amount) as Facture from customer customer, 
-address address, city city, payment payment
+address address, city city, payment payment 
 where payment.customer_id = customer.customer_id
 and customer.address_id = address.address_id
 and address.city_id = city.city_id
@@ -11,4 +11,3 @@ and city like 'Lubumbashi'
 group by customer.last_name;
 
 ```
-
