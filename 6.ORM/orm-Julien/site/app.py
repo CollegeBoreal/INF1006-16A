1a6f26a5628ef191c3fea43ddda3da22bc1e470a
Julien <julien-liebel@hotmail.com>
2016-11-25 13:23:19 -0500

    :)

--- 6.ORM/orm-Julien/site/app.py
+++ 6.ORM/orm-Julien/site/app.py
@@ -7,9 +7,27 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
+    return 'Hello, World'
+
+@app.route('/redis')
+def _redis():
    redis.incr('hits')
    return '{"hits":%s}' % redis.get('hits')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
