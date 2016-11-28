c2d36f3eb6da43e709a8bf96e343dc2371fcc3b7
Gary Uppiah <Gary Uppiah>
2016-11-28 13:30:30 -0500

    app

--- 6.ORM/orm-Gary/site/app.py
+++ 6.ORM/orm-Gary/site/app.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from redis import Redis
 
+import json
+
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
+   return 'Bonjour. Bienvenu.'
+
+@app.route('/redis')
+def _redis():
    redis.incr('hits')
-   return 'Hello World! I have been seen %s times.' % redis.get('hits')
+   return '{"hits":%s}' % redis.get('hits')
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
