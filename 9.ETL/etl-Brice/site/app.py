b9f36c8ae419ba4c6fae18512be2c0b31063679b
to-boreal <to-boreal@collegeboreal.ca>
2016-12-05 15:30:06 -0500

    Dark Jeans

--- 9.ETL/etl-Brice/site/app.py
+++ 9.ETL/etl-Brice/site/app.py
@@ -1,74 +1,36 @@
 from flask import Flask
-from redis import Redis
+from influxdb import InfluxDBClient
 
 import json
-import MySQLdb 
-
-cnx = MySQLdb.connect(user='ircodes', passwd='ircodes_1', host='mysql', db='ircodes')
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/ircodes')
-def _ircodes():
 
-   cursor = cnx.cursor()
-   list = []
-   arr = ""
-   query = ("SELECT * FROM BRAND")
+@app.route('/series')
+def series():
+    json_body = [
+    {
+        "measurement": "cpu_load_short",
+        "tags": {
+	            "host": "server01",
+                "region": "us-west"
+        },
+	        "time": "2009-11-10T23:00:00Z",
+	        "fields": {
+            "value": 0.64
+									               }
+										      }
+										   ]
 
-   cursor.execute(query)
-
-   cursor.close()
-   cnx.close()
-
-   for (id, brands) in cursor:
-      list.append({'id':id,'brands':brands})
-      arr = list[:] 
-   return(json.dumps(arr))
-
-@app.route('/brand')
-def _brand():
-   return 'brand page'
-
-@app.route('/modeltype')
-def _modeltype():
-   return 'modeltype page'
-
-@app.route('/model')
-def _model():
-   return 'model page'
-
-@app.route('/codenames')
-def _codenames():
-   return 'codenames page'
-
-@app.route('/codes')
-def _codesl():
-   return 'codes page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/redis')
-def _redis():
-   redis.incr('hits')
-   return '{"hits":%s}' % redis.get('hits')
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+   client = InfluxDBClient('influxdb', 8086, 'root', 'root', 'example')
+   client.create_database('example')
+   client.write_points(json_body)
+   result = client.query('select value from cpu_load_short;')
+   print("Result: {0}".format(result))
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
